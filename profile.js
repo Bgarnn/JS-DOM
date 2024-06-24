9ff1a1fe45667ecad6728c1f348f9c7d7331a5d9
Karn Buralek <karn.buralek904@gmail.com>
2024-06-24 17:00:13 +0700

    sidebar-icon

--- profile.js
+++ profile.js
@@ -3,129 +3,121 @@ import {} from "./dashBoardPage.js";
 export function isTag(node, tagName) {
     // Ensure we're dealing with an element node
     if (node.nodeType !== Node.ELEMENT_NODE) {
-      return false;
+        return false;
     }
-  
+
     // Compare tag names ignoring case (HTML tags are not case-sensitive)
     return node.tagName.toLowerCase() === tagName.toLowerCase();
-  }
+}
 
-  
-export class Profile extends HTMLElement{
-    constructor(){
+export class Profile extends HTMLElement {
+    constructor() {
         super();
     }
-    
-    // connectedCallback(){
-    //   this.shadow = this.attachShadow({ mode: "open" });
-    //   const div = document.createElement("div");
-    //   div.textContent = "Profile";
 
-    //   const css = document.createElement("link");
-    //   css.setAttribute("rel","stylesheet");
-    //   css.setAttribute("href","./components/profile.css");
-
-    //   this.loginElement = document.createElement("p");
-    //   this.loginElement.textContent = "Match History";
-    //   this.loginElement.addEventListener("click", () => {
-    //       const notificationElement = document.querySelector('main-page').shadowRoot.childNodes[0].shadowRoot.querySelector("notifi-cation").shadowRoot;
-    //       const matchHistory = document.createElement("match-history")
-    //       console.log(notificationElement);
-    //       if(notificationElement.childNodes.length == 3)
-    //         {
-    //           const nodeRemove = notificationElement.childNodes[2]
-    //           notificationElement.removeChild(nodeRemove)
-    //         }
-    //           // 
-    //       if(!isTag(notificationElement.lastChild,"match-history"))
-    //           notificationElement.appendChild(matchHistory);
-    //   });
-    //   // this.shadow.appendChild(this.loginElement)
-    //   div.appendChild(this.loginElement);
-    //   this.shadow.appendChild(css);
-    //   this.shadow.appendChild(div)
-    // }
     connectedCallback() {
-      this.shadow = this.attachShadow({ mode: "open" });
-    
-      // Create container div with id="container"
-      const container = document.createElement("div");
-      container.setAttribute("id", "container");
-    
-      // Create profile photo big div
-      const profilePhotoBigDiv = document.createElement("div");
-      profilePhotoBigDiv.setAttribute("id", "profile-photo-big");
-    
-      // Create img element for profile photo
-      const img = document.createElement("img");
-      img.setAttribute("src", "./images/profile-1.jpg");
-    
-      // Append img to profilePhotoBigDiv
-      profilePhotoBigDiv.appendChild(img);
-    
-      // Create a element for profile link
-      const profileLink = document.createElement("a");
-      profileLink.setAttribute("id", "profile-name");
-    
-      // Create h4 element for profile name
-      const h4 = document.createElement("h4");
-      h4.textContent = "Prem";
-    
-      // Append h4 to profileLink
-      profileLink.appendChild(h4);
-    
-      // Append profilePhotoBigDiv and profileLink to container
-      container.appendChild(profilePhotoBigDiv);
-      container.appendChild(profileLink);
-    
-      // Create the sidebar div
-      const sidebarDiv = document.createElement("div");
-      sidebarDiv.setAttribute("id", "side-bar");
-      sidebarDiv.textContent = "Profile";
-    
-      // Create and append CSS link
-      const css = document.createElement("link");
-      css.setAttribute("rel", "stylesheet");
-      css.setAttribute("href", "./components/profile.css");
-      this.shadow.appendChild(css); // Append CSS link to shadow DOM
-    
-      // Create and setup the "Match History" paragraph
-      this.loginElement = document.createElement("p");
-      this.loginElement.textContent = "Match History";
-      this.loginElement.addEventListener("click", () => {
-        const notificationElement = document.querySelector('main-page').shadowRoot.childNodes[0].shadowRoot.querySelector("notifi-cation").shadowRoot;
-        const matchHistory = document.createElement("match-history");
-        console.log(notificationElement);
-        if (notificationElement.childNodes.length == 3) {
-          const nodeRemove = notificationElement.childNodes[2];
-          notificationElement.removeChild(nodeRemove);
+        this.shadow = this.attachShadow({ mode: "open" });
+
+        // Create container div with id="container"
+        const container = document.createElement("div");
+        container.setAttribute("id", "container");
+
+        // Create profile photo big div
+        const profilePhotoBigDiv = document.createElement("div");
+        profilePhotoBigDiv.setAttribute("id", "profile-photo-big");
+        const img = document.createElement("img");
+        img.setAttribute("src", "./images/profile-1.jpg");
+        profilePhotoBigDiv.appendChild(img);
+
+        // Create a element for profile name
+        const profileName = document.createElement("a");
+        profileName.setAttribute("id", "profile-name");
+        const h4 = document.createElement("h4");
+        h4.textContent = "Prem";
+        profileName.appendChild(h4);
+        container.appendChild(profilePhotoBigDiv);
+        container.appendChild(profileName);
+
+        // Create the sidebar div
+        const sidebarDiv = document.createElement("div");
+        sidebarDiv.setAttribute("id", "side-bar");
+
+        // Function to create menu items
+        const createMenuItem = (iconClass, text, clickHandler, id = null) => {
+            const menuItem = document.createElement("a");
+            menuItem.classList.add("menu-item");
+            if (id) menuItem.id = id;
+
+            const span = document.createElement("span");
+            const icon = document.createElement("i");
+            icon.classList.add("uil", iconClass);
+            span.appendChild(icon);
+
+            const h3 = document.createElement("h3");
+            h3.textContent = text;
+
+            menuItem.appendChild(span);
+            menuItem.appendChild(h3);
+            menuItem.addEventListener("click", clickHandler);
+
+            return menuItem;
+        };
+
+        // Create and append menu items to sidebarDiv
+        sidebarDiv.appendChild(createMenuItem("uil-globe", "RANK 9", () => {}));
+        sidebarDiv.appendChild(createMenuItem("uil-chart-bar", "WIN 2 | LOSE 3", () => {}));
+        sidebarDiv.appendChild(createMenuItem("uil-file", "Match History", () => {
+            const notificationElement = document.querySelector('main-page').shadowRoot.childNodes[0].shadowRoot.querySelector("notifi-cation").shadowRoot;
+            const matchHistory = document.createElement("match-history");
+            console.log(notificationElement);
+            if (notificationElement.childNodes.length == 3) {
+                const nodeRemove = notificationElement.childNodes[2];
+                notificationElement.removeChild(nodeRemove);
+            }
+            if (!isTag(notificationElement.lastChild, "match-history")) {
+                notificationElement.appendChild(matchHistory);
+            }
+        }));
+        sidebarDiv.appendChild(createMenuItem("uil-user", "Account", () => {}));
+
+        // Create the "Notifications" menu item with notification count
+        const notificationsItem = createMenuItem("uil-bell", "Notifications", () => {}, "notifications");
+        const notificationCount = document.createElement("small");
+        notificationCount.classList.add("notification-count");
+        notificationCount.textContent = "new";
+        notificationsItem.querySelector("i").appendChild(notificationCount);
+        sidebarDiv.appendChild(notificationsItem);
+
+        sidebarDiv.appendChild(createMenuItem("uil-signout", "Log Out", () => {}));
+
+        // Create and append CSS link
+        const cssIcon = document.createElement("link");
+        cssIcon.setAttribute("rel", "stylesheet");
+        cssIcon.setAttribute("href", "https://unicons.iconscout.com/release/v3.0.6/css/line.css"); // Link to Unicons CSS
+        this.shadow.appendChild(cssIcon); // Append CSS link to shadow DOM
+
+        // Create and append CSS link
+        const css = document.createElement("link");
+        css.setAttribute("rel", "stylesheet");
+        css.setAttribute("href", "./components/profile.css");
+        this.shadow.appendChild(css); // Append CSS link to shadow DOM
+
+        // Append sidebarDiv to container
+        container.appendChild(sidebarDiv);
+
+        // Find existing div to insert container into
+        const existingDiv = this.shadow.querySelector('existing-div-selector'); // Replace 'existing-div-selector' with your actual selector
+
+        if (existingDiv) {
+            // Append container into existing div
+            existingDiv.appendChild(container);
+        } else {
+            // If existingDiv is not found, create a new div and append container to it
+            const newDiv = document.createElement('div');
+            newDiv.appendChild(container);
+            this.shadow.appendChild(newDiv);
         }
-        if (!isTag(notificationElement.lastChild, "match-history")) {
-          notificationElement.appendChild(matchHistory);
-        }
-      });
-      sidebarDiv.appendChild(this.loginElement); // Append "Match History" paragraph to profileDiv
-    
-      // Find existing div to insert container into
-      const existingDiv = this.shadow.querySelector('existing-div-selector'); // Replace 'existing-div-selector' with your actual selector
-    
-      if (existingDiv) {
-        // Append container into existing div
-        existingDiv.appendChild(container);
-      } else {
-        // If existingDiv is not found, create a new div and append container to it
-        const newDiv = document.createElement('div');
-        newDiv.appendChild(container);
-        this.shadow.appendChild(newDiv);
-      }
-    
-      // Append profileDiv (with "Profile" and "Match History" paragraph) into container
-      container.appendChild(sidebarDiv);
     }
-    
 }
 
 customElements.define("pro-file", Profile);
-
-
-
