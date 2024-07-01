e262c908f56e40a2a9de88060b6ffc0f1046879d
Karn Buralek <karn.buralek904@gmail.com>
2024-07-01 10:58:07 +0700

    reccom-friend

--- components/RecommentFriends.js
+++ components/RecommentFriends.js
@@ -1,3 +1,18 @@
+const Mock_list = [
+	{
+		name: 'Sarah',
+		profileImg: '../images/profile-2.jpg'
+	},
+	{
+		name: 'Jenny',
+		profileImg: '../images/profile-2.jpg'
+	},
+	{
+		name: 'Kim',
+		profileImg: '../images/profile-2.jpg'
+	}
+];
+
 export class RecommentFriends extends HTMLElement {
 	constructor() {
 		super();
@@ -6,9 +21,42 @@ export class RecommentFriends extends HTMLElement {
 
 	template = () => {
 		return `
+			<link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
 			<link rel="stylesheet" href="./components/recommentFriends.css">
-			<div>RecommentFriends</div>
+			
+			<div id="recommend-friend">
+				<div id="header">
+					<h4>Recommend Friends</h4>
+				</div>
+				<table>
+					<tbody>
+						${this.generateRows()}
+					</tbody>
+				</table>
+			</div>
 		`;
+	}
+
+	generateRows() {
+		return Mock_list.map(list => `
+			<tr>
+				<td>
+					<div id="profile">
+						<div id="profile-photo">
+							<img src="${list.profileImg}" alt="Profile Photo">
+						</div>
+						<div id="profile-name">
+							<p><b>${list.name}</b></p>
+						</div>
+					</div>
+				</td>
+				<td>
+					<button>
+						<i class="uil uil-user-plus"></i> Send Request
+					</button>
+				</td>
+			</tr>
+		`).join('');
 	}
 
 	connectedCallback() {
@@ -16,7 +64,7 @@ export class RecommentFriends extends HTMLElement {
 	}
 
 	disconnectedCallback() {
-		// console.log("delete")
+		console.log("delete recommend friend components");
 	}
 }
 
