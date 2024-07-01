53971126dbb7497e64a27e634273c10546310805
Karn Buralek <karn.buralek904@gmail.com>
2024-07-01 15:42:43 +0700

    all-friends

--- components/Friends.js
+++ components/Friends.js
@@ -1,3 +1,26 @@
+const Mock_list = [
+	{
+		name: 'Sarah',
+		profileImg: '../images/profile-2.jpg',
+		status: 'Online'
+	},
+	{
+		name: 'Jenny',
+		profileImg: '../images/profile-2.jpg',
+		status: 'Offline'
+	},
+	{
+		name: 'Lin',
+		profileImg: '../images/profile-2.jpg',
+		status: 'Offline'
+	},
+	{
+		name: 'Kim',
+		profileImg: '../images/profile-2.jpg',
+		status: 'Online'
+	}
+];
+
 import { changeNotification } from "./Utils.js";
 
 export class Friends extends HTMLElement {
@@ -8,13 +31,52 @@ export class Friends extends HTMLElement {
 
 	template = () => {
 		return `
+			<link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
 			<link rel="stylesheet" href="./components/friends.css">
 			
 			<div id="friends">
-				Friends
-				<button id="findFriendsButton">Find Friends</button>
+				<div id="header">
+					<h4>Friends</h4>
+					<button id="findFriendsButton">
+						<i class="uil uil-user-plus"></i> Find Friends
+					</button>
+				</div>
+				<table>
+					<tbody>
+						${this.generateRows()}
+					</tbody>
+				</table>
 			</div>
 		`;
+	};
+
+	generateRows() {
+		return Mock_list.map(list => `
+			<tr>
+				<td>
+					<div id="profile">
+						<div id="profile-photo">
+							<img src="${list.profileImg}" alt="Profile Photo">
+						</div>
+						<div id="profile-name">
+							<p><b>${list.name}</b></p>
+						</div>
+					</div>
+				</td>
+				<td>
+					<p class="${list.status === 'Online' ? 'status-online' : list.status === 'Offline' ? 'status-offline' : 'status-offline'}">
+						${list.status}
+					</p>
+				</td>
+				<td>
+					<div id="icon">
+						<i class="uil uil-user"></i>
+						<i class="uil uil-comment-dots"></i>
+						<i class="uil uil-upload"></i>
+					</div>
+				</td>
+			</tr>
+		`).join('');
 	}
 
 	connectedCallback() {
@@ -24,7 +86,9 @@ export class Friends extends HTMLElement {
 			changeNotification("recommends-friends");
 		});
 	}
+
 }
+
 
 // import { changeNotification } from "./Utils.js";
 
